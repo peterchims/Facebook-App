6932fb28eeaf7c346bee68d1dcc4cd44181d86c6
Peter4Tech <peter4tech@gmail.com>
2024-07-22 04:25:50 +0100

    left Navigaton Styling

--- src/pages/Mainarea/mainArea.js
+++ src/pages/Mainarea/mainArea.js
@@ -13,23 +13,41 @@ function MainArea() {
        Peter Lola
         </div>
         <div className='pro'>
+        <FaUserFriends className='fa-mess' />
+        <div> Friends </div>
+        </div>
+        <div className='pro'>
             <FaFontAwesomeFlag  className='flag' />
             <div> Pages </div>
         </div>
         <div className='pro'>
             <FaMedrt className='hrt' />
-            <div> Covid-19 Information Center</div>
+            <div> Covid-19 Center</div>
         </div>
         <div className='pro'>
         <FaFacebookMessenger className='fa-mess' />
         <div> Messenger Kids</div>
         </div>
-        <div className='pro'>
-        <FaUserFriends className='fa-mess' />
-        <div> Friends </div>
-        </div>
+        
         <div className='pro'>
         <FaAngleDown className='fa-ang' />
+        <div> See More</div>
+        </div>
+        <hr />
+        <div className='profiles'>
+            <img src="/images/d..jpg" alt="logo" className='profile-display'/>
+       <div> Tech Community </div>
+        </div>
+        <div className='profiles'>
+            <img src="/images/d..jpg" alt="logo" className='profile-display'/>
+       <div> Football Community </div>
+        </div>
+        <div className='profiles'>
+            <img src="/images/d..jpg" alt="logo" className='profile-display'/>
+       <div> Tech Community </div>
+        </div>
+        <div className='pros'>
+        <FaAngleDown />
         <div> See More</div>
         </div>
     </div>
